fff2a82cced888ff933617c658215f380b97e8e9
bala-amypo <bala@amypo.in>
2025-12-17 08:18:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/user.java
+++ src/main/java/com/example/demo/entity/user.java
@@ -1,4 +1,37 @@
 package.com.example.demo.entity;
 public class user{
-    
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+
+
+    public long getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+    public String getproductName(){
+        return productName;
+    }
+    public void setproductName(){
+        this.productName=productName;
+    }
+    public String getsku(){
+        return sku;
+    }
+    public void setsku(){
+        this.sku=sku;
+    }
+    public String getcategory(){
+        return password;
+    }
+    public void setcategory(){
+        this.password=password;
+    }
+    public LocalDateTime getcreatedAt(){
+        return createdAt;
+    }
 }
