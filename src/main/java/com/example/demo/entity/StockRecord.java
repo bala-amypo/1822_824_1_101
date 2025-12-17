1461062714fb694f276d0a45d7eeeb69edf239ac
bala-amypo <bala@amypo.in>
2025-12-17 06:40:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/StockRecord.java
+++ src/main/java/com/example/demo/entity/StockRecord.java
@@ -1,5 +1,36 @@
 
 package.com.example.demo.entity;
 public class StockRecord{
-    
+    private long id;
+    private int currentQuantity;
+    private int reorderThreshold;
+    private String lastUpdated;
+
+    public long getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+    public int getcurrentQuantity(){
+        return currentQuantity;
+    }
+    public void set(){
+        this.productName=productName;
+    }
+    public String getsku(){
+        return sku;
+    }
+    public void setsku(){
+        this.sku=sku;
+    }
+    public String getcategory(){
+        return category;
+    }
+    public void setcategory(){
+        this.category=category;
+    }
+    public String getcreatedAt(){
+        return createdAt;
+    }
 }
