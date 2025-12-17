b664e0f1fca9fd9afc77d4ce0df8a17d71fb8d1e
bala-amypo <bala@amypo.in>
2025-12-17 06:45:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/consumptionlog.java
+++ src/main/java/com/example/demo/entity/consumptionlog.java
@@ -2,5 +2,34 @@ package.com.example.demo.entity;
 
 public class consumptionlog{
     private Long id;
-    @Many-to-One
+    private int consumedQuantity;
+    private String consumedDate;
+
+    public long getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+    public int getconsumedQuantity(){
+        return consumed;
+    }
+    public void setproductName(){
+        this.productName=productName;
+    }
+    public String getsku(){
+        return sku;
+    }
+    public void setsku(){
+        this.sku=sku;
+    }
+    public String getcategory(){
+        return category;
+    }
+    public void setcategory(){
+        this.category=category;
+    }
+    public String getcreatedAt(){
+        return createdAt;
+    }
 }
