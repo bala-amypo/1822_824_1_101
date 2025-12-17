b01ca86c507d712b49598aaf903d3c7cdea3b0f6
bala-amypo <bala@amypo.in>
2025-12-17 06:25:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/product.java
+++ src/main/java/com/example/demo/entity/product.java
@@ -1,4 +1,10 @@
 package.com.example.demo.entity;
 public class product{
-    
+    private int id;
+    private String productName;
+    private String sku;
+    private String category;
+    private LocalDateTime createdAt;
+
+    public void
 }
