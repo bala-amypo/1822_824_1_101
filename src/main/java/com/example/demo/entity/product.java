33000016a673eb44c0444724af9ed9f23027fcb8
bala-amypo <bala@amypo.in>
2025-12-17 06:30:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/product.java
+++ src/main/java/com/example/demo/entity/product.java
@@ -6,5 +6,29 @@ public class product{
     private String category;
     private LocalDateTime createdAt;
 
-    public void
+    public int getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+    public String getproductName(){
+        return productName;
+    }
+    public void setproductName(){
+        this.productName=productName;
+    }
+    public String getsku(){
+        return sku;
+    }
+    public void setsku(){
+        this.sku=sku;
+    }
+    public String getcategory(){
+        return category;
+    }
+    public void setcategory(){
+        this.category=category;
+    }
+    public LocalDateTime getcreat
 }
