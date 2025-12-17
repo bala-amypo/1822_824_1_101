ceb83c65019a59405356184c1ed889c500a929a1
bala-amypo <bala@amypo.in>
2025-12-17 06:35:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/warehouse.java
+++ src/main/java/com/example/demo/entity/warehouse.java
@@ -3,4 +3,33 @@ public class warehouse{
    private long id;
    private String warehouseName;
    private String location; 
+   private String createdAt;
+
+   public long getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+    public String getproductName(){
+        return productName;
+    }
+    public void setproductName(){
+        this.productName=productName;
+    }
+    public String getsku(){
+        return sku;
+    }
+    public void setsku(){
+        this.sku=sku;
+    }
+    public String getcategory(){
+        return category;
+    }
+    public void setcategory(){
+        this.category=category;
+    }
+    public String getcreatedAt(){
+        return createdAt;
+    }
 }
