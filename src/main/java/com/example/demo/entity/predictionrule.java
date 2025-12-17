6124e8660fbea5bbb61b886040b58803b14f7f3c
bala-amypo <bala@amypo.in>
2025-12-17 06:50:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/predictionrule.java
+++ src/main/java/com/example/demo/entity/predictionrule.java
@@ -1,5 +1,37 @@
 package.com.example.demo.entity;
 
 public class predictionrule{
-    
+    private long id;
+    private String ruleName;
+    private int averageDaysWindow;
+    private int maxDailyUsage;
+    private String createdAt;
+
+    public long getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+    public String getruleName(){
+        return ruleNameName;
+    }
+    public void setproductName(){
+        this.ruleName=productName;
+    }
+    public String getsku(){
+        return sku;
+    }
+    public void setsku(){
+        this.sku=sku;
+    }
+    public String getcategory(){
+        return category;
+    }
+    public void setcategory(){
+        this.category=category;
+    }
+    public String getcreatedAt(){
+        return createdAt;
+    }
 }
