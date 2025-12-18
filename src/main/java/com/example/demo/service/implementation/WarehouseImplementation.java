f9286ef1e3834117c80f7c07f9ac09ebeded276b
bala-amypo <bala@amypo.in>
2025-12-18 12:49:37 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/WarehouseImplementation.java
+++ src/main/java/com/example/demo/service/implementation/WarehouseImplementation.java
@@ -1,3 +1,10 @@
-public class WarehouseImplementation{
-    
+@Service
+public class WarehouseImplementation implements WarehouseService{
+  @Autowired
+  WarehouseRepository obj;
+
+  public warehouse createwarehouse(warehouse ware){
+    return obj.save(ware);
+  }
+  public warehouse getwarehouse
 }
