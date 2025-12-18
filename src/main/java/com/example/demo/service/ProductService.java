9b5b6c7b9730f1f7aeb0b1b11d896b91a4bc3277
bala-amypo <bala@amypo.in>
2025-12-18 11:52:22 +0000

    User commit

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
-import com.example.demo.entity.;
+import com.example.demo.entity.product;
 import java.util.List;
 
-public interface ConsumptionLogService{
-    consumptionlog createconsumptionlog(consumptionlog conslog);
-    consumptionlog getconsumptionlogid(Long id);
-    List<consumptionlog> getAllconsumptionlog();
+public interface ProductService{
+    product createproduct(product prod);
+    product getproductid(Long id);
+    List<product> getAllproduct();
 }
--- src/main/java/com/example/demo/service/StockRecordService.java
+++ src/main/java/com/example/demo/service/StockRecordService.java
@@ -1,3 +1,9 @@
-public class StockRecordService{
-    
+package com.example.demo.service;
+import com.example.demo.entity.stockrecord;
+import java.util.List;
+
+public interface StockRecordService{
+    stockrecord createstockrecord(stockrecord stockrec);
+    stockrec getstockrecordid(Long id);
+    List<consumptionlog> getAllconsumptionlog();
 }
