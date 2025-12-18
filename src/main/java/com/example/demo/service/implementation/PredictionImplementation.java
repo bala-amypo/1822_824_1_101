438c00cbcfcba262d958cca29819548420b68fc7
bala-amypo <bala@amypo.in>
2025-12-18 13:04:37 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/PredictionImplementation.java
+++ src/main/java/com/example/demo/service/implementation/PredictionImplementation.java
@@ -1,3 +1,22 @@
-public class PredictionImplementation{
-    
+package com.example.demo.service.implementation;
+import java.util.List;
+import com.example.demo.service.PredictionService;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.predictionrule;
+import com.example.demo.repository.PredictionRulerepository;
+import org.springframework.beans.factory.annotation.Autowired;
+@Service
+public class PredictionImplementation implements UserService{
+  @Autowired
+  UserRepository obj;
+
+  public user creatuser(user user){
+    return obj.save(user);
+  }
+  public user getuserid(Long id){
+    return obj.findById(id).orElse(null);
+  }
+  public List<user>getAllusers(){
+    return obj.findAll();
+  }
 }
--- src/main/java/com/example/demo/service/implementation/ProductImplementation.java
+++ src/main/java/com/example/demo/service/implementation/ProductImplementation.java
@@ -2,21 +2,21 @@ package com.example.demo.service.implementation;
 import java.util.List;
 import com.example.demo.service.ProductService;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.user;
-import com.example.demo.repository.UserRepository;
+import com.example.demo.entity.product;
+import com.example.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 @Service
-public class UserImplementation implements UserService{
+public class ProductImplementation implements ProductService{
   @Autowired
-  UserRepository obj;
+ ProductRepository obj;
 
-  public user creatuser(user user){
-    return obj.save(user);
+  public product creatproduct(product prod){
+    return obj.save(prod);
   }
-  public user getuserid(Long id){
+  public product getproductid(Long id){
     return obj.findById(id).orElse(null);
   }
-  public List<user>getAllusers(){
+  public List<product>getAllproducts(){
     return obj.findAll();
   }
 }
