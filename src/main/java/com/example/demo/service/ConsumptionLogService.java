18eafeefa9a483169fb3c8884cb76ee0f71d175f
bala-amypo <bala@amypo.in>
2025-12-18 09:43:21 +0000

    User commit

--- src/main/java/com/example/demo/service/ConsumptionLogService.java
+++ src/main/java/com/example/demo/service/ConsumptionLogService.java
@@ -1,3 +1,3 @@
-// public class ConsumptionLogService{
+public class ConsumptionLogService{
     
-// }
+}
--- src/main/java/com/example/demo/service/PredictionService.java
+++ src/main/java/com/example/demo/service/PredictionService.java
@@ -1,3 +1,3 @@
-// public class PredictionService{
+public class PredictionService{
     
-// }
+}
