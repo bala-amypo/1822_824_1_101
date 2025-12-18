ebd5882c78f66bd94dd208d7367737f595f9413b
bala-amypo <bala@amypo.in>
2025-12-18 11:46:23 +0000

    User commit

--- src/main/java/com/example/demo/service/ConsumptionLogService.java
+++ src/main/java/com/example/demo/service/ConsumptionLogService.java
@@ -1,3 +1,7 @@
-public class ConsumptionLogService{
-    
+package com.example.demo.service;
+import 
+public interface ConsumptionLogService{
+    consumptionlog createconsumptionlog(consumptionlog conslog);
+    consumptionlog getconsumptionlogid(Long id);
+    List<consumptionlog> getAllconsumptionlog();
 }
