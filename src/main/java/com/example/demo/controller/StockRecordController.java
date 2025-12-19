6502d5b3a5edae49d20103f610691e5b12166df6
bala-amypo <bala@amypo.in>
2025-12-19 16:46:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/StockRecordController.java
+++ src/main/java/com/example/demo/controller/StockRecordController.java
@@ -1,3 +1,48 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.StockRecord;
+import com.example.demo.service.StockRecordService;
+
+@RestController
 public class StockRecordController{
-    
+    @Autowired
+    StockRecordService storec;
+
+    @PostMapping("/addStock")
+    public StockRecord addStockRecord(@RequestBody product product){
+        return prod.addProducts(product);
+    }
+
+    @GetMapping("/getProducts")
+    public List<product> getProducts(){
+        return prod.getProducts();
+    }
+
+    @GetMapping("/getProduct/{id}")
+    public product getProductById(@PathVariable Long id){
+        return prod.getProductById(id);
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public String deleteProductById(@PathVariable Long id){
+        product product=prod.getProductById(id);
+        if(product!=null){
+            prod.deleteProductById(id);
+            return "Product deleted successfully";
+        }
+        else{
+            return "Product not found.";
+        }
+    }
+
+
 }
