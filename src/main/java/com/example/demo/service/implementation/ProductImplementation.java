f75d8dd448f01ece6946bda31d13759df55ac7dc
bala-amypo <bala@amypo.in>
2025-12-20 06:53:03 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/ProductImplementation.java
+++ src/main/java/com/example/demo/service/implementation/ProductImplementation.java
@@ -1,22 +1,36 @@
-package com.example.demo.service.implementation;
+package com.example.demo.service;
+
 import java.util.List;
-import com.example.demo.service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Product;
 import com.example.demo.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-@Service
-public abstract class ProductImplementation implements ProductService{
-  @Autowired
- private ProductRepository obj;
 
-  public Product createProduct(Product prod){
-    return obj.save(prod);
-  }
-  public Product getProductid(Long id){
-    return obj.findById(id).orElse(null);
-  }
-  public List<Product>getAllProducts(){
-    return obj.findAll();
-  }
+@Service
+public class ProductImplementation implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
 }
