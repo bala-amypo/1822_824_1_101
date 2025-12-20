1b766541429ceb391c073853d37f6b5c22bb52cf
bala-amypo <bala@amypo.in>
2025-12-20 06:43:06 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/ConsumptionLogImplementation.java
+++ src/main/java/com/example/demo/service/implementation/ConsumptionLogImplementation.java
@@ -1,23 +1,36 @@
-package com.example.demo.service.implementation;
+package com.example.demo.service;
+
 import java.util.List;
-import com.example.demo.service.ConsumptionLogService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.ConsumptionLog;
 import com.example.demo.repository.ConsumptionLogRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-@Service
-public abstract class ConsumptionLogImplementation implements ConsumptionLogService {
-    @Autowired
-    private ConsumptionLogRepository obj;
 
-  public ConsumptionLog createConsumptionLog(ConsumptionLog clog){
-    return obj.save(clog);
-  }
-  public ConsumptionLog getConsumptionLogid(Long id){
-    return obj.findById(id).orElse(null);
-  }
-  public List<ConsumptionLog>getAllConsumptionLogs(){
-    return obj.findAll();
-  }
+@Service
+public class ConsumptionLogImplementation implements ConsumptionLogService {
+
+    @Autowired
+    private ConsumptionLogRepository consumptionLogRepository;
+
+    @Override
+    public ConsumptionLog addConsumptionLog(ConsumptionLog log) {
+        return consumptionLogRepository.save(log);
+    }
+
+    @Override
+    public List<ConsumptionLog> getAllConsumptionLogs() {
+        return consumptionLogRepository.findAll();
+    }
+
+    @Override
+    public ConsumptionLog getConsumptionLogById(Long id) {
+        return consumptionLogRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteConsumptionLogById(Long id) {
+        consumptionLogRepository.deleteById(id);
+    }
 }
- 
