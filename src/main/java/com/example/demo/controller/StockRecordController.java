f3111f937ae1fb8585c5b8e2fa140beea7e160b5
bala-amypo <bala@amypo.in>
2025-12-20 06:49:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/StockRecordController.java
+++ src/main/java/com/example/demo/controller/StockRecordController.java
@@ -1,103 +1,42 @@
-// package com.example.demo.controller;
-
-// import java.util.List;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import com.example.demo.entity.StockRecord;
-// import com.example.demo.service.StockRecordService;
-
-// @RestController
-// public class StockRecordController{
-//     @Autowired
-//     StockRecordService storec;
-
-//     @PostMapping("/addStock")
-//     public StockRecord addStockRecord(@RequestBody StockRecord stock){
-//         return storec.addStockRecord(stock);
-//     }
-
-//     @GetMapping("/getStockRecord")
-//     public List<StockRecord> getStockRecords(){
-//         return storec.getStockRecords();
-//     }
-
-//     @GetMapping("/getStockRecord/{id}")
-//     public StockRecord getStockRecordtById(@PathVariable Long id){
-//         return storec.getStockRecordById(id);
-//     }
-
-//     @DeleteMapping("/deleteStockRecord/{id}")
-//     public String deleteStockRecordById(@PathVariable Long id){
-//         StockRecord StockRecord=storec.getStockRecordById(id);
-//         if(StockRecord!=null){
-//             prod.deleteStockRecordById(id);
-//             return "StockRecord deleted successfully";
-//         }
-//         else{
-//             return "StockRecord not found.";
-//         }
-//     }
-
-
-// }
-
-
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Long;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.StockRecord;
 import com.example.demo.service.StockRecordService;
 
 @RestController
-@RequestMapping("/StockRecord")
+@RequestMapping("/stockrecords")
 public class StockRecordController {
 
     @Autowired
-    private StockRecordService stockService;
+    private StockRecordService stockRecordService;
 
     // CREATE
     @PostMapping
-    public ResponseEntity<StockRecord> addStockRecord(@RequestBody StockRecord stock) {
-        return ResponseEntity.ok(stockService.addStockRecord(stock));
+    public StockRecord addStockRecord(@RequestBody StockRecord stockRecord) {
+        return stockRecordService.addStockRecord(stockRecord);
     }
 
     // READ ALL
     @GetMapping
-    public ResponseEntity<List<StockRecord>> getStockRecord() {
-        return ResponseEntity.ok(stockService.getStockRecord());
+    public List<StockRecord> getAllStockRecords() {
+        return stockRecordService.getAllStockRecords();
     }
 
     // READ BY ID
     @GetMapping("/{id}")
-    public ResponseEntity<StockRecord> getStockRecordById(@PathVariable Long id) {
-        StockRecord StockRecord =stockService.getStockRecordById(id);
-        if (StockRecord == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(StockRecord);
+    public StockRecord getStockRecordById(@PathVariable Long id) {
+        return stockRecordService.getStockRecordById(id);
     }
 
     // DELETE
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteStockRecordById(@PathVariable Long id) {
-        StockRecord  StockRecord =  stockService.getStockRecordById(id);
-        if ( StockRecord == null) {
-            return ResponseEntity.notFound().build();
-        }
-        stockService.deleteStockRecordById(id);
-        return ResponseEntity.ok("Stock deleted successfully");
+    public String deleteStockRecordById(@PathVariable Long id) {
+        stockRecordService.deleteStockRecordById(id);
+        return "Stock record deleted successfully";
     }
 }
-
-
