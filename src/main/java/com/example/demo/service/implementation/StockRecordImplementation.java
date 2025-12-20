972e4055083427c5d6b8ec623dbc3275535a9957
bala-amypo <bala@amypo.in>
2025-12-20 06:50:00 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/StockRecordImplementation.java
+++ src/main/java/com/example/demo/service/implementation/StockRecordImplementation.java
@@ -1,22 +1,36 @@
-package com.example.demo.service.implementation;
+package com.example.demo.service;
+
 import java.util.List;
-import com.example.demo.service.StockRecordService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.StockRecord;
 import com.example.demo.repository.StockRecordRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-@Service
-public abstract class StockRecordImplementation implements StockRecordService{
-  @Autowired
-  private StockRecordRepository obj;
 
-  public StockRecord createStockRecord(StockRecord stockrec){
-    return obj.save(stockrec);
-  }
-  public StockRecord getStockRecordid(Long id){
-    return obj.findById(id).orElse(null);
-  }
-  public List<StockRecord>getAllStockRecords(){
-    return obj.findAll();
-  }
+@Service
+public class StockRecordImplementation implements StockRecordService {
+
+    @Autowired
+    private StockRecordRepository stockRecordRepository;
+
+    @Override
+    public StockRecord addStockRecord(StockRecord stockRecord) {
+        return stockRecordRepository.save(stockRecord);
+    }
+
+    @Override
+    public List<StockRecord> getAllStockRecords() {
+        return stockRecordRepository.findAll();
+    }
+
+    @Override
+    public StockRecord getStockRecordById(Long id) {
+        return stockRecordRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteStockRecordById(Long id) {
+        stockRecordRepository.deleteById(id);
+    }
 }
