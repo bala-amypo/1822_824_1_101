2b4409ee6662922bb64874cfe344412ebb3089c2
bala-amypo <bala@amypo.in>
2025-12-20 05:57:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/StockRecordController.java
+++ src/main/java/com/example/demo/controller/StockRecordController.java
@@ -46,3 +46,58 @@
 
 
 // }
+
+
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.StockRecord;
+import com.example.demo.service.StockRecordService;
+
+@RestController
+@RequestMapping("/StockRecord")
+public class StockRecordController {
+
+    @Autowired
+    private StockRecordService stockService;
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<StockRecord> addStockRecord(@RequestBody StockRecord stock) {
+        return ResponseEntity.ok(stockService.addStockRecord(stock));
+    }
+
+    // READ ALL
+    @GetMapping
+    public ResponseEntity<List<StockRecord>> getStockRecords() {
+        return ResponseEntity.ok(stockService.getStockRecords());
+    }
+
+    // READ BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<StockRecord> getProductById(@PathVariable Long id) {
+        Product Product =prodService.getProductById(id);
+        if (Product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(Product);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProductById(@PathVariable Long id) {
+        Product  Product =  prodService.getProductById(id);
+        if ( Product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        prodService.deleteProductById(id);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
+}
+
+
