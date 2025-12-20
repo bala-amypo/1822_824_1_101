354e63689980d64e8ad60d745f1508e5367cf967
bala-amypo <bala@amypo.in>
2025-12-20 06:54:54 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/WarehouseImplementation.java
+++ src/main/java/com/example/demo/service/implementation/WarehouseImplementation.java
@@ -1,22 +1,36 @@
-package com.example.demo.service.implementation;
+package com.example.demo.service;
+
 import java.util.List;
-import com.example.demo.service.WarehouseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Warehouse;
 import com.example.demo.repository.WarehouseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-@Service
-public abstract class WarehouseImplementation implements WarehouseService{
-  @Autowired
-  private WarehouseRepository obj;
 
-  public Warehouse createWarehouse(Warehouse ware){
-    return obj.save(ware);
-  }
-  public Warehouse getWarehouseid(Long id){
-    return obj.findById(id).orElse(null);
-  }
-  public List<Warehouse>getAllWarehouses(){
-    return obj.findAll();
-  }
+@Service
+public class WarehouseImplementation implements WarehouseService {
+
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+
+    @Override
+    public Warehouse addWarehouse(Warehouse warehouse) {
+        return warehouseRepository.save(warehouse);
+    }
+
+    @Override
+    public List<Warehouse> getAllWarehouses() {
+        return warehouseRepository.findAll();
+    }
+
+    @Override
+    public Warehouse getWarehouseById(Long id) {
+        return warehouseRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteWarehouseById(Long id) {
+        warehouseRepository.deleteById(id);
+    }
 }
