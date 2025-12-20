8d3c65cc3c5e265529688789e850d8580418c191
bala-amypo <bala@amypo.in>
2025-12-20 06:24:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -51,7 +51,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java,util.Long;
+import java.util.Long;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
