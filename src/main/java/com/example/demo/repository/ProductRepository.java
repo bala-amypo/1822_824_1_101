f4ec22fea2d019be6dbc19ae20e211646615c0da
bala-amypo <bala@amypo.in>
2025-12-26 09:04:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,8 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.example.demo.model.Product;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
--- src/main/java/com/example/demo/repository/WarehouseRepository.java
+++ src/main/java/com/example/demo/repository/WarehouseRepository.java
@@ -1,11 +1,1 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Warehouse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
-
-    Optional<Warehouse> findByWarehouseName(String warehouseName);
-}
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {}
