a43697f1039b4fd492c684b92844a51addc89d77
bala-amypo <bala@amypo.in>
2025-12-26 06:50:24 +0000

    User commit

--- src/main/java/com/example/demo/util/DateUtil.java
+++ src/main/java/com/example/demo/util/DateUtil.java
@@ -1,3 +1,23 @@
-public class DateUtil{
-    
+package com.example.demo.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class DateUtil {
+
+    private DateUtil() {
+        // utility class – prevent object creation
+    }
+
+    public static LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+
+    public static LocalDate today() {
+        return LocalDate.now();
+    }
+
+    public static boolean isFutureDate(LocalDate date) {
+        return date.isAfter(LocalDate.now());
+    }
 }
