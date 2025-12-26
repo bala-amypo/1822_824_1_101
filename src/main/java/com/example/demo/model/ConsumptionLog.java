1ba02987ef4da3adf66018c249862a6351f1f702
bala-amypo <bala@amypo.in>
2025-12-26 09:02:47 +0000

    User commit

--- src/main/java/com/example/demo/model/ConsumptionLog.java
+++ src/main/java/com/example/demo/model/ConsumptionLog.java
@@ -1,42 +1,23 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import java.time.LocalDateTime;
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
 @Entity
-public class ConsumptionLog{
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConsumptionLog {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private int consumedQuantity;
-    private LocalDateTime consumedDate;
 
-    public long getId(){
-        return id;
-    }
-    public void setId(long id){
-        this.id=id;
-    }
-    public int getConsumedQuantity(){
-        return consumedQuantity;
-    }
-    public void setConsumedQuantity(int consumedQuantity){
-        this.consumedQuantity=consumedQuantity;
-    }
-    public LocalDateTime getConsumedDate(){
-        return consumedDate;
-    }
-    public void setConsumedDate(LocalDateTime consumedDate){
-        this.consumedDate=consumedDate;
-    }
-     public ConsumptionLog(long id,int consumedQuantity,LocalDateTime consumedDate){
-        this.id=id;
-        this.consumedQuantity=consumedQuantity;
-        this.consumedDate=consumedDate;
-    }
-    public ConsumptionLog(){
+    private Integer consumedQuantity;
+    private LocalDate consumedDate;
 
-    }
-   
+    @ManyToOne
+    private StockRecord stockRecord;
 }
