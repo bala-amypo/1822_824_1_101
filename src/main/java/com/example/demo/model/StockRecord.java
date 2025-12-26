3b7a3b29f2f5892a98c66a899ae92a2e11231b05
bala-amypo <bala@amypo.in>
2025-12-26 09:02:25 +0000

    User commit

--- src/main/java/com/example/demo/model/StockRecord.java
+++ src/main/java/com/example/demo/model/StockRecord.java
@@ -1,45 +1,30 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
+
 @Entity
-public class StockRecord{
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"product_id", "warehouse_id"})
+})
+public class StockRecord {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private int currentQuantity;
     private int reorderThreshold;
     private LocalDateTime lastUpdated;
 
-    public long getId(){
-        return id;
-    }
-    public void setId(long id){
-        this.id=id;
-    }
-    public int getCurrentQuantity(){
-        return currentQuantity;
-    }
-    public void setCurrentQuantity(int currentQuantity){
-        this.currentQuantity=currentQuantity;
-    }
-    public int getReorderThreshold(){
-        return reorderThreshold;
-    }
-    public void setReorderThreshold(int reorderThreshold){
-        this.reorderThreshold=reorderThreshold;
-    }
-    public LocalDateTime getLastUpdated(){
-        return lastUpdated;
-    }
-    public void setLastUpdated(LocalDateTime lastUpdated){
-        this.lastUpdated=lastUpdated;
-    }
-    public StockRecord(){
-        
-    }
-    
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Warehouse warehouse;
 }
