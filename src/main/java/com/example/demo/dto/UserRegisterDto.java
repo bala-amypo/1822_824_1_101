35d0b89e1441e6068a6e4ec8529fb43ec63deec0
bala-amypo <bala@amypo.in>
2025-12-26 06:48:07 +0000

    User commit

--- src/main/java/com/example/demo/dto/UserRegisterDto.java
+++ src/main/java/com/example/demo/dto/UserRegisterDto.java
@@ -1,3 +1,18 @@
-public class UserRegisterDto{
-    
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterDto {
+
+    private String name;
+    private String email;
+    private String password;
+    private Set<String> roles;
 }
