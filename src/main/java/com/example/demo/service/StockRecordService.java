3e9659ba9171b64d129ee990677998bd88bc1106
bala-amypo <bala@amypo.in>
2025-12-26 09:08:25 +0000

    User commit

--- src/main/java/com/example/demo/service/StockRecordService.java
+++ src/main/java/com/example/demo/service/StockRecordService.java
@@ -1,16 +1,13 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.StockRecord;
-
+import com.example.demo.model.StockRecord;
 import java.util.List;
 
 public interface StockRecordService {
 
-    StockRecord createStockRecord(Long productId, Long warehouseId, StockRecord record);
+    StockRecord createStockRecord(Long productId, Long warehouseId, StockRecord stockRecord);
 
     StockRecord getStockRecord(Long id);
 
     List<StockRecord> getRecordsBy_product(Long productId);
-
-    List<StockRecord> getRecordsByWarehouse(Long warehouseId);
 }
