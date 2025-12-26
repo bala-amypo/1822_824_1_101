504d04bb9fb6295699205afbf4e9caec15af668b
bala-amypo <bala@amypo.in>
2025-12-26 13:25:32 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
@@ -1,40 +1,24 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.Role;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public User register(User user) {
-
-        // default values for exam safety
-        user.setCreatedAt(LocalDateTime.now());
-
-        if (user.getRoles() == null || user.getRoles().isEmpty()) {
-            user.setRoles(Set.of(Role.ROLE_USER));
-        }
-
         return userRepository.save(user);
     }
 
     @Override
     public User findByEmail(String email) {
-        Optional<User> user = userRepository.findByEmail(email);
-        return user.orElse(null);
+        return userRepository.findByEmail(email).orElse(null);
     }
 }
