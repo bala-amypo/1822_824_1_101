0f54659350c0a8d1d89dc2780dd865483b1752ba
bala-amypo <bala@amypo.in>
2025-12-26 09:03:04 +0000

    User commit

--- src/main/java/com/example/demo/model/PredictionRule.java
+++ src/main/java/com/example/demo/model/PredictionRule.java
@@ -1,59 +1,23 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
-import jakarta.persistence.Column;
+
 @Entity
-public class PredictionRule{
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PredictionRule {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long id;
-    @Column(unique=true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String ruleName;
     private int averageDaysWindow;
+    private int minDailyUsage;
     private int maxDailyUsage;
     private LocalDateTime createdAt;
-
-    public long getId(){
-        return id;
-    }
-    public void setId(long id){
-        this.id=id;
-    }
-    public String getRuleName(){
-        return ruleName;
-    }
-    public void setRuleName(String ruleName){
-        this.ruleName=ruleName;
-    }
-    public int  getAverageDaysWindows(){
-        return averageDaysWindow;
-    }
-    public void setAverageDaysWindow(int averageDaysWindow){
-        this.averageDaysWindow=averageDaysWindow;
-    }
-    public int getMaxDailyUsage(){
-        return maxDailyUsage;
-    }
-    public void setMaxDailyUsage(int maxDailyUsage){
-        this.maxDailyUsage=maxDailyUsage;
-    }
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt){
-        this.createdAt=createdAt;
-    }
-     public PredictionRule(long id,String ruleName,int averageDaysWindow,int maxDailyUsage, LocalDateTime createdAt){
-        this.id=id;
-        this.ruleName=ruleName;
-        this.averageDaysWindow=averageDaysWindow;
-        this.maxDailyUsage=maxDailyUsage;
-        this.createdAt=createdAt;
-    }
-    public PredictionRule(){
-
-    }
 }
