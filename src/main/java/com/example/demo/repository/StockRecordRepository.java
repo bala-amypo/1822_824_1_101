98820aba90a9b7074d317a9113795917a8a4c816
bala-amypo <bala@amypo.in>
2025-12-26 09:05:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/StockRecordRepository.java
+++ src/main/java/com/example/demo/repository/StockRecordRepository.java
@@ -1,13 +1,3 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.StockRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
 public interface StockRecordRepository extends JpaRepository<StockRecord, Long> {
-
     List<StockRecord> findByProductId(Long productId);
-
-    List<StockRecord> findByWarehouseId(Long warehouseId);
 }
