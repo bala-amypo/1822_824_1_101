1f54e6a12ea3c05f80216220654a9fafae39d56b
bala-amypo <bala@amypo.in>
2025-12-26 09:07:48 +0000

    User commit

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,5 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Product;
+import java.util.List;
+
 public interface ProductService {
-    Product createProduct(Product p);
+
+    Product createProduct(Product product);
+
     Product getProduct(Long id);
+
     List<Product> getAllProducts();
 }
