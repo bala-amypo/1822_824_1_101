928ba7931bb182db260cf0c4c38ef16199f7cafe
bala-amypo <bala@amypo.in>
2025-12-26 09:01:51 +0000

    User commit

--- src/main/java/com/example/demo/model/Warehouse.java
+++ src/main/java/com/example/demo/model/Warehouse.java
@@ -1,54 +1,21 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotNull;
+
 @Entity
-public class Warehouse{
-   @Id
-   @GeneratedValue(strategy=GenerationType.IDENTITY)
-   private long id;
-   @Column(unique=true)
-   private String warehouseName;
-   @NotNull
-   private String location; 
-   private LocalDateTime createdAt;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warehouse {
 
-   public long getId(){
-        return id;
-    }
-    public void setId(long id){
-        this.id=id;
-    }
-    public String getWarehouseName(){
-        return warehouseName;
-    }
-    public void setWarehouseName(String warehouseName){
-        this.warehouseName=warehouseName;
-    }
-    public String getLocation(){
-        return location;
-    }
-    public void setLocation(String location){
-        this.location=location;
-    }
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt){
-        this.createdAt=createdAt;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Warehouse(long id,String warehouseName,String location,LocalDateTime createdAt){
-        this.id=id;
-        this.warehouseName=warehouseName;
-        this.location=location;
-        this.createdAt=createdAt;
-    }
-    public Warehouse(){
-
-    }
+    private String warehouseName;
+    private String location;
+    private LocalDateTime createdAt;
 }
