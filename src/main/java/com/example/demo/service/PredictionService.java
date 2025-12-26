84e1a22d007dda732f6ff96117fa8a96f117a74e
bala-amypo <bala@amypo.in>
2025-12-26 13:07:53 +0000

    User commit

--- src/main/java/com/example/demo/service/PredictionService.java
+++ src/main/java/com/example/demo/service/PredictionService.java
@@ -1,12 +1,16 @@
 package com.example.demo.service;
 
 import com.example.demo.model.PredictionRule;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public interface PredictionService {
+
     PredictionRule createRule(PredictionRule rule);
+
     List<PredictionRule> getAllRules();
+
+    // ⚠️ MUST be LocalDate (NOT LocalDateTime)
     LocalDate predictRestockDate(Long productId);
 }
-
