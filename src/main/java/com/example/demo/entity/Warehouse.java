d4a91ffa513a89ceff035f4233d2f1f0b8f3eae5
bala-amypo <bala@amypo.in>
2025-12-26 08:10:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warehouse.java
+++ src/main/java/com/example/demo/entity/Warehouse.java
@@ -1,25 +1,54 @@
 package com.example.demo.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import java.time.LocalDateTime;
-
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
 @Entity
-@Table(name = "warehouses", uniqueConstraints = @UniqueConstraint(columnNames = "warehouseName"))
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class Warehouse {
+public class Warehouse{
+   @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+   private long id;
+   @Column(unique=true)
+   private String warehouseName;
+   @NotNull
+   private String location; 
+   private LocalDateTime createdAt;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+   public long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id=id;
+    }
+    public String getWarehouseName(){
+        return warehouseName;
+    }
+    public void setWarehouseName(String warehouseName){
+        this.warehouseName=warehouseName;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public void setLocation(String location){
+        this.location=location;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
 
-    private String warehouseName;
+    public Warehouse(long id,String warehouseName,String location,LocalDateTime createdAt){
+        this.id=id;
+        this.warehouseName=warehouseName;
+        this.location=location;
+        this.createdAt=createdAt;
+    }
+    public Warehouse(){
 
-    private String location;
-
-    private LocalDateTime createdAt;
+    }
 }
