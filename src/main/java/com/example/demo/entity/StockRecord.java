e9dd93d49ff051a314b916e45501d62738a37388
bala-amypo <bala@amypo.in>
2025-12-26 06:19:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/StockRecord.java
+++ src/main/java/com/example/demo/entity/StockRecord.java
@@ -1,45 +1,33 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
-@Entity
-public class StockRecord{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long id;
-    private int currentQuantity;
-    private int reorderThreshold;
-    private LocalDateTime lastUpdated;
 
-    public long getId(){
-        return id;
-    }
-    public void setId(long id){
-        this.id=id;
-    }
-    public int getCurrentQuantity(){
-        return currentQuantity;
-    }
-    public void setCurrentQuantity(int currentQuantity){
-        this.currentQuantity=currentQuantity;
-    }
-    public int getReorderThreshold(){
-        return reorderThreshold;
-    }
-    public void setReorderThreshold(int reorderThreshold){
-        this.reorderThreshold=reorderThreshold;
-    }
-    public LocalDateTime getLastUpdated(){
-        return lastUpdated;
-    }
-    public void setLastUpdated(LocalDateTime lastUpdated){
-        this.lastUpdated=lastUpdated;
-    }
-    public StockRecord(){
-        
-    }
-    
+@Entity
+@Table(
+        name = "stock_records",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"product_id", "warehouse_id"})
+)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Warehouse warehouse;
+
+    private Integer currentQuantity;
+    private Integer reorderThreshold;
+
+    private LocalDateTime lastUpdated;
 }
