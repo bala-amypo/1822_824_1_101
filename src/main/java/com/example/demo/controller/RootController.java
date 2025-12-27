03be4f9e03ff2a7c410b739772aca9a83fdd3284
bala-amypo <bala@amypo.in>
2025-12-27 13:31:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/RootController.java
+++ src/main/java/com/example/demo/controller/RootController.java
@@ -12,3 +12,17 @@
 //         return ResponseEntity.badRequest().body("Invalid endpoint");
 //     }
 // }
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.badRequest().body("Invalid endpoint");
+    }
+}
