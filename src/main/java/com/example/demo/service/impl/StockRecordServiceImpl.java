c3aa94d3fe434178563711cee5432e7c95dc45a7
bala-amypo <bala@amypo.in>
2025-12-27 03:58:05 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StockRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StockRecordServiceImpl.java
@@ -1,33 +1,33 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.StockRecord;
-import com.example.demo.service.StockRecordService;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
+import com.example.demo.model.StockRecord;
+import com.example.demo.repository.StockRecordRepository;
+import com.example.demo.service.StockRecordService;
 
 @Service
 public class StockRecordServiceImpl implements StockRecordService {
 
-    @Override
-    public StockRecord createStockRecord(Long productId, Long warehouseId, StockRecord stockRecord) {
-        stockRecord.setLastUpdated(LocalDateTime.now());
-        return stockRecord;
+    private final StockRecordRepository stockRecordRepository;
+
+    public StockRecordServiceImpl(StockRecordRepository stockRecordRepository) {
+        this.stockRecordRepository = stockRecordRepository;
     }
 
     @Override
-    public StockRecord getStockRecord(Long id) {
-        StockRecord sr = new StockRecord();
-        sr.setId(id);
-        sr.setCurrentQuantity(0);
-        sr.setReorderThreshold(0);
-        return sr;
-    }
+    public StockRecord createStockRecord(StockRecord stockRecord) {
 
-    @Override
-    public List<StockRecord> getRecordsBy_product(Long productId) {
-        return Collections.emptyList();
+        boolean exists = stockRecordRepository
+                .existsByProductIdAndWarehouseId(
+                        stockRecord.getProduct().getId(),
+                        stockRecord.getWarehouse().getId()
+                );
+
+        if (exists) {
+            throw new RuntimeException("Stock record already exists");
+        }
+
+        return stockRecordRepository.save(stockRecord);
     }
 }
