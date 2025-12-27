537d354a0c31e55a519702364f4772b42f8e386b
bala-amypo <bala@amypo.in>
2025-12-27 13:31:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/RootController.java
+++ src/main/java/com/example/demo/controller/RootController.java
@@ -1,14 +1,14 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class RootController {
+// @RestController
+// public class RootController {
 
-    @GetMapping("/")
-    public ResponseEntity<String> home() {
-        return ResponseEntity.badRequest().body("Invalid endpoint");
-    }
-}
+//     @GetMapping("/")
+//     public ResponseEntity<String> home() {
+//         return ResponseEntity.badRequest().body("Invalid endpoint");
+//     }
+// }
