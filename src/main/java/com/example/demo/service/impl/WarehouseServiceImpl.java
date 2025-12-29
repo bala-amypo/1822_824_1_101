97611bfe997ce28bbb5b5782374653b0229a3325
bala-amypo <bala@amypo.in>
2025-12-29 09:00:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/WarehouseServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/WarehouseServiceImpl.java
@@ -1,32 +1,27 @@
 package com.example.demo.service.impl;
-
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Warehouse;
+import com.example.demo.repository.WarehouseRepository;
 import com.example.demo.service.WarehouseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class WarehouseServiceImpl implements WarehouseService {
+    private final WarehouseRepository repo;
 
     @Override
-    public Warehouse createWarehouse(Warehouse warehouse) {
-        return warehouse;
+    public Warehouse createWarehouse(Warehouse w) {
+        if (repo.findByWarehouseName(w.getWarehouseName()).isPresent()) 
+            throw new IllegalArgumentException("Duplicate name");
+        return repo.save(w);
     }
-
     @Override
     public Warehouse getWarehouse(Long id) {
-        Warehouse warehouse = new Warehouse();
-        warehouse.setId(id);
-        warehouse.setWarehouseName("Main warehouse");
-        warehouse.setLocation("Chennai");
-        return warehouse;
+        return repo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Warehouse not found"));
     }
-
     @Override
-    public List<Warehouse> getAllWarehouses() {
-        return new ArrayList<>();
-    }
+    public List<Warehouse> getAllWarehouses() { return repo.findAll(); }
 }
-
