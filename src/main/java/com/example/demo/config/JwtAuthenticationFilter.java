3127464cd496c9fe234ffaf11696dcfaf6d5305e
bala-amypo <bala@amypo.in>
2025-12-29 08:35:25 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -2,57 +2,22 @@ package com.example.demo.config;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import com.example.demo.security.CustomUserDetailsService;
-
 import java.io.IOException;
 
-@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtProvider jwtProvider;
+    private final JwtProvider provider;
 
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
+    public JwtAuthenticationFilter(JwtProvider provider) {
+        this.provider = provider;
+    }
 
     @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            jakarta.servlet.http.HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
-
-        String header = request.getHeader("Authorization");
-
-        if (header != null && header.startsWith("Bearer ")) {
-            String token = header.substring(7);
-
-            if (jwtProvider.validateToken(token)) {
-                String email = jwtProvider.getEmailFromToken(token);
-
-                var userDetails = userDetailsService.loadUserByUsername(email);
-
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(
-                                userDetails,
-                                null,
-                                userDetails.getAuthorities()
-                        );
-
-                authentication.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-
-                SecurityContextHolder.getContext()
-                        .setAuthentication(authentication);
-            }
-        }
+    protected void doFilterInternal(HttpServletRequest request,
+                                    jakarta.servlet.http.HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
 
         filterChain.doFilter(request, response);
     }
