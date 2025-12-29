ecedb049814636e618755f0f4bf5a564bf4f0ff2
bala-amypo <bala@amypo.in>
2025-12-29 03:51:29 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -17,6 +20,14 @@ public class OpenApiConfig {
     public OpenAPI openAPI() {
 
         return new OpenAPI()
+
+                // 🌐 Server URL (Swagger base URL)
+                .servers(List.of(
+                        new Server()
+                                .url("https://9026.32procr.amypo.ai")
+                                .description("Production Server")
+                ))
+
                 // API basic info
                 .info(new Info()
                         .title("Smart Inventory Restock API")
